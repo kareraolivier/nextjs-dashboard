98edf3b0fd96207c32ac7e4457a90842d46e2503
kareraolivier <kareraolivier@gmail.com>
2024-05-06 16:12:45 +0200

    feat: fix the deployed version

--- auth.config.ts
+++ auth.config.ts
@@ -1,4 +1,9 @@
 import type { NextAuthConfig } from 'next-auth';
+import { redirect } from 'next/navigation';
+
+export async function GET(request: Request) {
+  redirect('https://nextjs-dashboard-sampro.vercel.app/');
+}
 
 export const authConfig = {
   pages: {
